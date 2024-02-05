96b8b9603d6f2c1b3fd1cca3c5671f4141a41a2b
tan90xx <592026773@qq.com>
2024-02-05 20:56:26 +0800

    https://github.com/bshall/hifigan

---
+++ hifigan/hifigan/__init__.py
@@ -0,0 +1,7 @@
+from .generator import HifiganGenerator
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
